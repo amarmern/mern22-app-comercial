88b8d50409fe358945f7dd961556587ab5bb9874
amarmern <amarmca51@gmail.com>
2022-11-18 19:01:41 +0530

    addded product screen implemnation and helmet

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 const productsReducer = (state, action) => {
   switch (action.type) {
@@ -43,6 +44,9 @@ export default function HomeScreen() {
 
   return (
     <div>
+      <Helmet>
+        <title>Amazona</title>
+      </Helmet>
       <h1>Feature of Products</h1>
       <div className="products">
         {loading ? (
