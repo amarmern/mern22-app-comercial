257af2ad5a8eccfa857e6deb9b1c76abe545a3e0
amarmern <amarmca51@gmail.com>
2022-11-20 14:22:16 +0530

    created sign in api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,10 +19,20 @@ mongose
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 
 app.use('/api/products', productRouter);
 
+app.use('/api/users', userRouter);
+
+//if any error cought by express asyc handler then it will be handle by below middle ware.
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
